6773a92a860e81024c61b252c6a1a1db2fa06908
Anne-Sophie Vere <mdln.vere@gmail.com>
2025-04-07 12:03:32 +0200

    implements tailwind and assets

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export const profile = () => {
+// render
+    return (
+        <View>
+            <Text>profile</Text>
+        </View>
+    )
+}
+
+export default profile
---
+++ app/sign-in.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const SignIn = () => {
+// render
+    return (
+        <View>
+            <Text>SignIn </Text>
+        </View>
+    )
+}
+
+export default SignIn
